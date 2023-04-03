27af28e9f64ace57ee1dbb90271b3531577bfc38
domidanke <domi.tajak@gmail.com>
2023-04-03 20:52:12 +0200

    add basic filter

--- lib/views/feature/shared/events_page.dart
+++ lib/views/feature/shared/events_page.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:event_finder/models/consts.dart';
 import 'package:event_finder/models/enums.dart';
 import 'package:event_finder/models/event.dart';
 import 'package:event_finder/services/firestore/event_doc.service.dart';
@@ -8,6 +9,8 @@ import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
+import '../../../theme/theme.dart';
+
 class EventsPage extends StatefulWidget {
   const EventsPage({super.key});
 
@@ -16,6 +19,8 @@ class EventsPage extends StatefulWidget {
 }
 
 class _EventsPageState extends State<EventsPage> {
+  bool _today = false;
+  String? _selectedGenre;
   @override
   void initState() {
     super.initState();
@@ -23,6 +28,7 @@ class _EventsPageState extends State<EventsPage> {
 
   @override
   Widget build(BuildContext context) {
+    print('REBUILD ROOT');
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
@@ -57,16 +63,66 @@ class _EventsPageState extends State<EventsPage> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(8),
-          child: FirestoreListView<Event>(
-            emptyBuilder: (context) {
-              return const Center(
-                child: Text('Keine Events'),
-              );
-            },
-            query: _getQuery(),
-            itemBuilder: (context, snapshot) {
-              return EventCard(event: snapshot.data());
-            },
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _today = !_today;
+                      });
+                    },
+                    child: Chip(
+                      backgroundColor: _today ? primaryColor : null,
+                      label: const Text('Heute'),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  if (_selectedGenre != null)
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _selectedGenre = null;
+                        });
+                      },
+                      child: Chip(
+                        label: Row(
+                          children: [
+                            Text(_selectedGenre!),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            const Icon(
+                              Icons.cancel,
+                              size: 16,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                ],
+              ),
+              Expanded(
+                flex: 10,
+                child: FirestoreListView<Event>(
+                  emptyBuilder: (context) {
+                    return const Center(
+                      child: Text('Keine Events'),
+                    );
+                  },
+                  query: _getQuery(),
+                  itemBuilder: (context, snapshot) {
+                    return EventCard(event: snapshot.data());
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
@@ -74,14 +130,55 @@ class _EventsPageState extends State<EventsPage> {
   }
 
   Query<Event> _getQuery() {
-    return EventDocService().eventsCollection.orderBy('date');
+    print('GENRE: $_selectedGenre');
+    var query = EventDocService().eventsCollection.orderBy('date');
+
+    if (_today) {
+      final now = DateTime.now();
+      final startOfDay = DateTime(now.year, now.month, now.day);
+      final endOfDay = DateTime(now.year, now.month, now.day + 1);
+      query = query
+          .where('date', isLessThanOrEqualTo: endOfDay)
+          .where('date', isGreaterThanOrEqualTo: startOfDay);
+    }
+
+    if (_selectedGenre != null) {
+      query = query.where('genre', isEqualTo: _selectedGenre);
+    }
+
+    return query;
   }
 
   void _showFiltersSheet() {
+    _selectedGenre = _selectedGenre ?? genres.first;
     showModalBottomSheet<String>(
       context: context,
-      builder: (BuildContext context) => const Center(
-        child: Text('Filter'),
+      builder: (BuildContext context) => Container(
+        height: 200,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            DropdownButton<String>(
+              value: _selectedGenre,
+              icon: const Icon(Icons.arrow_drop_down_rounded),
+              onChanged: (String? value) {
+                setState(() {
+                  _selectedGenre = value!;
+                });
+                Navigator.pop(context);
+              },
+              items: genres.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(
+                    value,
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                );
+              }).toList(),
+            )
+          ],
+        ),
       ),
     );
   }
